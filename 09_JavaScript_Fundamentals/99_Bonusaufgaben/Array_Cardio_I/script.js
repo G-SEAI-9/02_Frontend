cb8307fe53f3a9c429e28477b4c7c1713ceb6f24
Stephan Ullmann <stephan.ullmann@posteo.de>
2026-09-08 14:39:55 +0200

    bonus array cardios

---
+++ 09_JavaScript_Fundamentals/99_Bonusaufgaben/Array_Cardio_I/script.js
@@ -0,0 +1,32 @@
+// Deklariere zwei Variablen, eine mit const und eine mit let
+// Gib jeder einen beliebigen Wert (String, Zahl, Array, Objekt, Boolean)
+
+// Gib die Werte beider Variablen in der Konsole aus
+
+// Erstelle eine Funktionsdelkaration (mit dem Schlüsselwort function) ohne Parameter, die einen String in der Konsole ausgibt. Rufe sie dann auf.
+
+// Erstelle eine Funktionsdeklaration, die 2 Parameter entgegennimmt und einen String **ZURÜCKGIBT**, der diese 2 Parameter mithilfe eines Template Literals enthält. Gib dann den Rückgabewert der Funktion in der Konsole aus.
+
+// Wandle die 2 Funktionen aus den vorherigen Übungen in Arrow-Functions um.
+
+// Erstelle ein Array mit 5 Elementen.
+
+// Gib die ersten 2 Werte in der Konsole aus
+
+// Gib das letzte Element des Arrays in der Konsole aus
+
+// Gib die Elemente 3 und 4 des Arrays in der Konsole aus
+
+// Erstelle ein Objekt mit den Schlüsseln "name", "alter", "beruf" und "hobbys" (hobbys soll ein Array sein)
+
+// Gib den Namen in der Konsole aus
+
+// Gib den Beruf und das Alter in der Konsole aus
+
+// Gib alle Hobbys in der Konsole aus
+
+// Gib nur das erste Hobby in der Konsole aus
+
+// Erstelle ein Array mit 3 Objekten. Die Objekte sollen dieselben Schlüssel wie das Objekt aus der vorherigen Übung haben.
+
+// Iteriere durch das Array von Objekten und gib alle Namen in der Konsole aus
