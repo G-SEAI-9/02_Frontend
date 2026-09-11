3b6909c08db51b5989deff24c703aa773d01b363
Stephan Ullmann <stephan.ullmann@posteo.de>
2026-09-11 08:53:02 +0200

    dom manipulation übungen starter

---
+++ 10_Web_APIs/02_DOM_Korrekturen/003-Adding-Events/script.js
@@ -0,0 +1,21 @@
+// Array of 10 random tasks as strings
+const tasks = [
+  'Complete the project',
+  'Attend the meeting',
+  'Write a report',
+  'Review the Code',
+  'Fix the bugs',
+  'Update the documentation',
+  'Plan the next sprint',
+  'Conduct user testing',
+  'Optimize the performance',
+  'Design',
+];
+
+// ============================================================
+//   **JavaScript Tasks**:
+//     *   Attach an event to the first button to create a new
+//       `li` in the `ul` with a random task from the provided array.
+//     *   Make sure you scroll to the last task so
+//       the last one is always visible!
+// ============================================================
