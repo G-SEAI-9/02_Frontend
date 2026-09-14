9b876fa225d72aa03d948b462137164469c26e63
Stephan Ullmann <stephan.ullmann@posteo.de>
2026-09-14 13:21:10 +0200

    fetch übungen

---
+++ 10_Web_APIs/04_fetch_Korrekturen/01_todos/script.js
@@ -0,0 +1,50 @@
+const ulElement = document.getElementById('todo-list');
+
+function renderTodos(data) {
+  const todos = data
+    .map((todo) => {
+      return `<li  class=" rounded-2xl px-4 py-2 flex flex-wrap items-center gap-x-4 gap-y-2
+                before:content-[''] before:block before:w-5 before:h-5
+                before:rounded-full before:border-2 ${
+                  todo.completed ? 'before:bg-green-400 before:border-green-400' : 'before:border-gray-400'
+                }" >${todo.title}</li>`;
+    })
+    .join('');
+
+  return todos;
+}
+
+async function fetchData(url) {
+  const response = await fetch(url);
+  if (!response.ok) throw new Error('no data fetched');
+  console.log(response);
+  return await response.json();
+}
+
+async function main() {
+  try {
+    const todoData = await fetchData('https://jsonplaceholder.typicode.com/todos');
+    const todoHTML = renderTodos(todoData);
+    ulElement.insertAdjacentHTML('beforeend', todoHTML);
+  } catch {
+    ulElement.insertAdjacentHTML('beforeend', '<p>Error: Try again later</p>');
+  }
+}
+
+main();
+
+// fetch("https://jsonplaceholder.typicode.com/todos")
+//   .then((response) => response.json())
+//   .then((data) => {
+//     data.slice(0, 10).forEach((todo) => {
+//       const li = document.createElement("li");
+//       li.textContent = todo.title;
+
+//       // check completed
+//       if (todo.completed) {
+//         li.style.textDecoration = "line-through";
+//       }
+
+//       ulElement.appendChild(li);
+//     });
+//   });
