1bcdeb68ec1dc575c56fc72fedd1a70c24b20360
Stephan Ullmann <stephan.ullmann@posteo.de>
2026-09-25 13:34:31 +0200

    react fundamentals

---
+++ 12_React_Intro/02_React_Fundamentals_Uebungen/03_displaying_data/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1>Displaying Data</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ 12_React_Intro/02_React_Fundamentals_Uebungen/05_rendering_lists/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1>Rendering Lists</h1>
+    </>
+  );
+}
+
+export default App;
